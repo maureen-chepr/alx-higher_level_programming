ff68776f6717002f0d36bf8f9b7e1dc20b45bcd1
maureen-chepr <maureenchep95@gmail.com>
2023-10-03 07:32:01 -0700

    Updated

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 for x in range(9):
     for y in range(x+1, 10):
-        print("{:02d}, ".format(x*10 + y), end="")
-print("89")
+        if x+1 != 9:
+            print("{:d}{:d}, ".format(x, y), end="")
+        else:
+            print("{:d}{:d}".format(x, y))
+            x += 1
