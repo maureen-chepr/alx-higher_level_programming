d8f1d2009739dcaf8533b6bb0086a768ba94f420
maureen-chepr <maureenchep95@gmail.com>
2023-10-05 08:55:05 -0700

    Infinite addition

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+
+    from sys import argv
+    total_args = 0
+    for i in range(1, len(argv)):
+        total_args += int(argv[i])   
+    print(total_args)
