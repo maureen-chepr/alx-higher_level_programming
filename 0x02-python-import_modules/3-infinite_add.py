91972eb3a11da4896a207dc9d386d916e9fbf895
maureen-chepr <maureenchep95@gmail.com>
2023-10-05 09:00:58 -0700

    removed trailing whitespace

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,5 +4,5 @@ if __name__ == '__main__':
     from sys import argv
     total_args = 0
     for i in range(1, len(argv)):
-        total_args += int(argv[i])   
-    print(total_args)
+        total_args += int(argv[i])
+        print(total_args)
