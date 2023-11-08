dad532dcd966171492d10657d140ec9883140993
maureen-chepr <maureenchep95@gmail.com>
2023-11-08 01:10:01 -0800

    add items

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,10 +9,17 @@ from os import path
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-try:
-    arg_list = load_from_json_file("add_item.json")
-except ValueError:
-    arg_list = []
+def add_items():
+    """
+        adds all arguments to a Python list,
+        and then saves them to a file
+    """
+    try:
+        arg_list = load_from_json_file("add_item.json")
+    except ValueError:
+        arg_list = []
 
-arg_list.extend(argv[1:])
-save_to_json_file(arg_list, "add_item.json")
+    arg_list.extend(argv[1:])
+    save_to_json_file(arg_list, "add_item.json")
+
+add_items()
