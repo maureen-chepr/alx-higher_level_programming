0252cf14deb782b5917fa560170341253b2d7bba
maureen-chepr <maureenchep95@gmail.com>
2023-11-07 23:42:08 -0800

    To JSON string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+    JSON representation of an object (string)
+"""
+
+
+def to_json_string(my_obj):
+    """
+        function that returns the JSON representation of an object (string)
+    """
+
+    return json.dumps(my_obj)
