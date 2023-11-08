0a0be9789e91afb9a919b128c0074c54155a70f0
maureen-chepr <maureenchep95@gmail.com>
2023-11-08 02:20:28 -0800

    Student to JSON with filter

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+    class Student module
+"""
+
+
+class Student():
+    """
+        class Student that defines a student
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+            initialization of class student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+            retrieves a dictionary representation
+            of a Student instance
+        """
+        if type(attrs) is list and all([type(idx) == str for idx in attrs]):
+            return {k: v for k, v in self.__dict__.items() if k in attrs}
+        return self.__dict__
