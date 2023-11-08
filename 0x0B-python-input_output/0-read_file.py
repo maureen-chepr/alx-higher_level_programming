f53fd6b61ee8763cb43ad1b77f976a17c32dc20c
maureen-chepr <maureenchep95@gmail.com>
2023-11-07 22:56:56 -0800

    Read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+    read function
+"""
+
+
+def read_file(filename=""):
+    """
+        function that reads a text file (UTF8)
+        and prints it to stdout
+    """
+
+     with open(filename, "r", encoding="utf-8") as myFile:
+        print(myFile.read(), end="")
