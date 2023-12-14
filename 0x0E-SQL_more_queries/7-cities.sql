1896b52dd8fd5f3715922b450398a2beaee1c288
maureen-chepr <maureenchep95@gmail.com>
2023-12-14 15:16:35 +0300

    Cities table

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,2 +1,3 @@
 -- creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa)
-CREATE TABLE IF NOT EXISTS cities (id INT UNIQUE AUTO_INCREMENT NOT NULL, state_id INT NOT NULL, name VARCHAR(256) NOT NULL, FOREIGN KEY (state_id) REFERENCES states(id));
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (id int NOT NULL PRIMARY KEY AUTO_INCREMENT, state_id int NOT NULL , name varchar(256) NOT NULL, FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id));
