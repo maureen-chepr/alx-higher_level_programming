76cc08a884e597da040e355cbf024f04c467db97
maureen-chepr <maureenchep95@gmail.com>
2024-01-08 21:39:24 +0300

    sort sbi

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,26 +1,15 @@
 #!/usr/bin/node
-/*
- * sorting second biggest integer
- */
+
 const secondBiggestInteger = (arr = []) => {
   if (arr.length <= 1) {
     console.log(0);
     return;
   }
 
-  let largestNum = arr[0];
-  let secondLargestNum = arr[0];
+  const sortedArr = arr.sort((a, b) => b - a);
 
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > largestNum) {
-      secondLargestNum = largestNum;
-      largestNum = arr[i];
-    } else if (arr[i] > secondLargestNum && arr[i] !== largestNum) {
-      secondLargestNum = arr[i];
-    }
-  }
-
-  console.log(secondLargestNum);
+  console.log(sortedArr[1]);
 };
+
 const args = process.argv.slice(2).map(Number);
 secondBiggestInteger(args);
