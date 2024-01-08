deec5eb472d68bab5aa4cbe5369b9cd075dae5f7
maureen-chepr <maureenchep95@gmail.com>
2024-01-08 21:24:00 +0300

    Second biggest!

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2).map(Number);
+
+const secondBiggestInteger = (arr = []) => {
+  if (arr.length <= 1) {
+    console.log(0);
+    return;
+  }
+
+  let largestNum = arr[0];
+  let secondLargestNum = arr[0];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > largestNum) {
+      secondLargestNum = largestNum;
+      largestNum = arr[i];
+    } else if (arr[i] > secondLargestNum && arr[i] !== largestNum) {
+      secondLargestNum = arr[i];
+    }
+  }
+
+  console.log(secondLargestNum);
+};
+secondBiggestInteger(args);
