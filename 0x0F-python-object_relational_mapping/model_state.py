c60cd862a3b0863ae7feb619132caf6dc9249058
maureen-chepr <maureenchep95@gmail.com>
2024-01-14 12:22:23 +0300

    trial 2

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -12,12 +12,3 @@ class State(Base):
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
-
-if __name__ == "__main__":
-    if len(sys.argv) != 5:
-        sys.exit(1)
-
-    username, password, host, database = sys.argv[1:5]
-    statement = 'mysql+mysqldb://{}:{}@{}/{}'.format(username, password, host, database)
-    engine = create_engine(statement, pool_pre_ping=True)
-    Base.metadata.create_all(engine)
