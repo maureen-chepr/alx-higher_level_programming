5bb79be2ab64f408e25cc8a8a3803765c7d4a58d
maureen-chepr <maureenchep95@gmail.com>
2024-01-14 11:53:22 +0300

    try using cmd args

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -2,13 +2,22 @@
 """
 links to the MySQL table states
 """
-from sqlalchemy import Column, Integer, String
+import sys
+from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
-
 
 class State(Base):
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        sys.exit(1)
+
+    username, password, host, database = sys.argv[1:5]
+    statement = 'mysql+mysqldb://{}:{}@{}/{}'.format(username, password, host, database)
+    engine = create_engine(statement, pool_pre_ping=True)
+    Base.metadata.create_all(engine)
