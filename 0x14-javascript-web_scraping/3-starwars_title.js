e0ca1b846d251baec84db064d6cc1e5c0ae67fe7
maureen-chepr <maureenchep95@gmail.com>
2024-02-20 22:21:31 +0300

    fix

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,22 +1,13 @@
 #!/usr/bin/node
 
 const request = require('request');
-const apiUrl = process.argv[2];
-const wedgeAntillesId = '18';
+const movieId = process.argv[2];
+const apiUrl = 'https://swapi-api.hbtn.io/api/films/' + movieId;
 
 request.get(apiUrl, function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
-    const filmsData = JSON.parse(body);
-    let count = 0;
-
-    for (const film of filmsData.results) {
-      if (film.characters.includes(`https://swapi-api.hbtn.io/api/people/${wedgeAntillesId}/`)) {
-        count++;
-      }
-    }
-
-    console.log(count);
+    console.log(JSON.parse(body).title);
   }
 });
